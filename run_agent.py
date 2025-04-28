e4d62aa24f9e09ac282ce2bfc89cab818dbcfd08
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2025-04-28 16:11:24 -0400

    update

--- run_agent.py
+++ run_agent.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     GOOGLE_API_KEY = "AIzaSyChmF0glFkgHwSatkXfFQIBEbaW8-PQCUE"
     gemini_model_name = "gemini-2.0-flash"
-    taskname = "easy-5-gpt-parsed"
+    taskname = "easy-0-gpt-parsed"
     task = "tasks/"+taskname+".json"
     
 
